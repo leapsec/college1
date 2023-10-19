ac90e991fc7da10aa2340d3248a2aa92ae9defea
leapsec <karanramrakhyani18@gmail.com>
2023-10-19 20:57:19 +0530

    c1

--- nodejs/assignment/ex8.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-async function getrp(url){
-    var resp = await fetch(url);
-    resp = await resp.text(); 
-    return resp;
-}
-const resp = await getrp('http://google.com/?q=Spider-Man');
-console.log(resp);
-//we need to add "type":"module" in the package.json file
